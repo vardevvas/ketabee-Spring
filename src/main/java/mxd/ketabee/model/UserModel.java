3b7fec8201dd8e98855353e659fc91f0c9cde533
vardevvas <vardevvas@gmail.com>
2022-11-09 16:11:30 +0200

    nice

--- src/main/java/mxd/ketabee/model/UserModel.java
+++ src/main/java/mxd/ketabee/model/UserModel.java
@@ -1,12 +1,21 @@
 package mxd.ketabee.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
+@Table(name = "Users")
 @Data
 public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String username;
     private String password;
@@ -16,5 +25,6 @@ public class UserModel {
     private String phone;
     private String mailid;
     private String usertype;
+    private String role;
 
 }
