f9a5be58b58e59798d57af7674632e84e9f3ed7d
vardevvas <vardevvas@gmail.com>
2022-11-22 08:58:49 +0200

    authentication first draft

--- src/main/java/mxd/ketabee/security/AppSecurityConfig.java
+++ src/main/java/mxd/ketabee/security/AppSecurityConfig.java
@@ -2,24 +2,44 @@ package mxd.ketabee.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.RequiredArgsConstructor;
+import mxd.ketabee.filter.AuthFilter;
+import mxd.ketabee.serviceimp.UserServiceImp;
 
 @Configuration
 @EnableWebSecurity
-public class AppSecurityConfig {
+@RequiredArgsConstructor
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserServiceImp userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    // private final AuthFilter authFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+        http.addFilter(new AuthFilter(authenticationManagerBean()));
+    }
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http
-                .csrf().disable()
-                .authorizeRequests(auth -> {
-                    auth.antMatchers("/").permitAll();
-                })
-                .httpBasic(Customizer.withDefaults())
-                .build();
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
